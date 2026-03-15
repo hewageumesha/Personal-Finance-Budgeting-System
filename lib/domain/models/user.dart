19fdafea962e87241f6e31d26cb9bd634df4d962
ImalKesara <imalkesara2001@gmail.com>
2026-03-15 16:45:44 +0530

    user model and installed sqlite package

---
+++ lib/domain/models/user.dart
@@ -0,0 +1,54 @@
+class User {
+  final int id;
+  final String email;
+  final String username;
+  final String baseCurrency;
+
+  User(
+      {required this.id,
+      required this.email,
+      required this.username,
+      required this.baseCurrency // LKR or USD
+
+      });
+
+
+  // ===========================================================================
+  //  this handle DTOs work too , you can seprate this too when application become complex and larger(data layer / model
+  //  ), for now we use both inside this
+  // ===========================================================================
+
+  /*
+  write to database
+  this converts dart objects into map that sqlite can save
+  */
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "email": email,
+      "username": username,
+      "base_currency": baseCurrency
+    };
+  }
+
+  /*
+    Read from database
+  */
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+        id: map['id'] as int,
+        email: map['email'] as String,
+        username: map['username'] as String,
+        baseCurrency: map['base_currency'] as String);
+  }
+
+  User copyWith(
+      {int? id, String? email, String? username, String? baseCurrency}) {
+    return User(
+        id: id ?? this.id,
+        username: username ?? this.username,
+        email: email ?? this.email,
+        baseCurrency: baseCurrency ?? this.baseCurrency);
+  }
+}
