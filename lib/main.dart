afc4a96e83c1e38f40a5e6d6490fb265e6eab328
ImalKesara <imalkesara2001@gmail.com>
2026-04-12 18:25:28 +0530

    Removed sensitive file

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:personal_finance_budgeting_system/features/authentication/data/repositories/auth_repository_Impl.dart';
 import 'package:personal_finance_budgeting_system/features/authentication/presentation/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:personal_finance_budgeting_system/routes/app_router.dart';
 import 'package:personal_finance_budgeting_system/shared/styles/app_theme.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   try {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
